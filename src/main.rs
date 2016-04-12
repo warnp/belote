ba6a6cb353ee7672ecd1b4bbe293c3dc202ee7c8
Romain <romain.assie@supinfo.com>
2016-04-12 22:31:14 +0200

    feat(creation.jeu) : First function to create cards deck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+fn creer_des_cartes() -> Vec<String>{
+    let mut resultat = vec![];
+
+    for i in 0..32 {
+
+        match i {
+            0 ... 8 => resultat.push("carreaux".to_string()),
+            9 ... 16 => resultat.push("trefle".to_string()),
+            17 ... 24 => resultat.push("pique".to_string()),
+            25 ... 32 => resultat.push("coeur".to_string()),
+            _ => (),
+        }
+    }
+
+    resultat
+}
+
 fn main() {
-    println!("Hello, world!");
+    let jeu = creer_des_cartes();
+
+    for i in jeu {
+        println!("{:#?}", i);
+    }
 }
